b3860c29b7abf286b67d054e8b216cdd18e24ae3
yogap02 <yogalaxy69@gmail.com>
2025-06-23 13:22:28 +0700

    initial commit

--- public/env.js
+++ public/env.js
@@ -1,7 +1,7 @@
 window.ENV = {
-  "FIREBASE_API_KEY": "",
-  "FIREBASE_AUTH_DOMAIN": "",
-  "FIREBASE_PROJECT_ID": "",
-  "FIREBASE_APP_ID": "",
-  "FIREBASE_DATABASE_URL": ""
+  "FIREBASE_API_KEY": "AIzaSyBesMzALgsJEQgLOfADb7w1_WAo8UNyrts",
+  "FIREBASE_AUTH_DOMAIN": "kibod-7315b.firebaseapp.com",
+  "FIREBASE_PROJECT_ID": "kibod-7315b",
+  "FIREBASE_APP_ID": "1:679544733711:web:80d12cdaa3e08794ec637a",
+  "FIREBASE_DATABASE_URL": "https://kibod-7315b-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
